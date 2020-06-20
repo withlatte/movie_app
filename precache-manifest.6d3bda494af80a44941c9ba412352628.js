c96d9a1b6b5c9035ba0f6df63b6ff44bc09575fd
withlatte <withlatte@naver.com>
2020-06-20 21:57:24 +0900

    Updates

---
+++ precache-manifest.6d3bda494af80a44941c9ba412352628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011777a672c29d6ce25879ef374d60a4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "bd66371dce5fed5cee63",
+    "url": "/movie_app/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd66371dce5fed5cee63",
+    "url": "/movie_app/static/js/main.eb3d7abc.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
