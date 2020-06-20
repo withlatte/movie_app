5cb30e1b61400c98a31b2334c4b97991fcaeb1f6
jpark_office <withlatte@naver.com>
2020-06-20 10:22:21 +0900

    #4.2 done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 // npm install axios
 // axios = fetch() 와 같은 역할을 한다. url을 불러온다.
 // https://yts-proxy.now.sh/list_movies.json
@@ -13,7 +14,16 @@ class App extends React.Component {
   // async 와 await : axios를 통해 url을 불러올때 약간의 시간이 걸린다.
   // axios 작업이 종료될 때까지 기다리는 방법이다. ES6에 추가된 기능
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      // 아래 구문은 movies.data.data.movies 와 동일하다.
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+    console.log(movies);
   };
 
   componentDidMount() {
@@ -22,8 +32,24 @@ class App extends React.Component {
 
   render() {
     // 같은 class 안의 state 오브젝트에 있는 isLoading을 불러와 사용할 수 있도록 선언
-    const { isLoading } = this.state;
-    return <div> {isLoading ? "Loading..." : "We are ready!"} </div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                year={movie.year}
+                rating={movie.rating}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
